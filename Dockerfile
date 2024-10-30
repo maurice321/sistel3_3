fcaed58f19bf83c1a0b1c6fc621a4274ce13df0c
maurice321 <jvillanob@gmail.com>
2024-10-30 16:20:08 -0500

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.12
+
+WORKDIR /app
+
+COPY . /app
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+EXPOSE 5000
+
+CMD ["python", "app.py"]
---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://base_datos_2_user:OKu4d2EKBUi9NMOsyknkUWn7QGAj0ZPc@dpg-cs8180q3esus73conma0-a.oregon-postgres.render.com/base_datos_2'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
